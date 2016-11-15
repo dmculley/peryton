372cf78e693b398496d7177c977d21d9b3a1fbaf
dmculley <dave@lcs-fast.com>
2016-11-15 12:15:36 +0000

    Adding formwork for incoming n-s solver

--- peryton/helpers.py
+++ peryton/helpers.py
@@ -7,7 +7,14 @@
 """
 
 from firedrake import *
+from collections import namedtuple
+from mpi4py import MPI
+from pyop2.profiling import timed_stage
 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#   I N P U T   /   O U T P U T   F U N C T I O N S   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def info_out(msg, colour=None, bold=False, underline=False, comm=COMM_WORLD):
     """ Prints the passed message
@@ -41,3 +48,158 @@ def info_out(msg, colour=None, bold=False, underline=False, comm=COMM_WORLD):
             if underline:
                 msg = '\033[4m' + msg + '\033[0m'
         print(msg)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#   F I R E D R A K E   E X T E N S I O N S   #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+def pointwise_norm(function):
+    """ Pointwise norm (rather than the integral of the L2norm over the
+    domain as is returned by Firedrake.norm()
+    """
+    return sqrt(inner(function, function))
+
+
+def tensor_jump(vector_field, normal):
+    """ Equivalent of jump(scalar, n) for vectorial quantities. The
+    inbuilt firedrake jump(vector, n) is as below but uses inner - i.e.
+    returning a scalar... this here returns a vector.
+    """
+    return outer(vector_field('+'), normal('+')) \
+    + outer(vector_field('-'), normal('-'))
+
+
+ElementContinuity = namedtuple("ElementContinuity",
+                               ["dg", "horizontal_dg", "vertical_dg"])
+"""A named tuple describing the continuity of an element."""
+
+
+def element_continuity(fiat_element):
+    """Return an :class:`ElementContinuity` instance with the
+    continuity of a given element.
+    :arg fiat_element: The fiat element to determine the continuity
+        of.
+    :returns: A new :class:`ElementContinuity` instance.
+    """
+    import FIAT
+    cell = fiat_element.get_reference_element()
+
+
+    if isinstance(cell, FIAT.reference_element.TensorProductCell):
+        # Pull apart
+        horiz = element_continuity(fiat_element.A).dg
+        vert = element_continuity(fiat_element.B).dg
+        return ElementContinuity(dg=horiz and vert,
+                                 horizontal_dg=horiz,
+                                 vertical_dg=vert)
+    else:
+        edofs = fiat_element.entity_dofs()
+        dim = cell.get_spatial_dimension()
+        dg = True
+        for i in range(dim - 1):
+            if any(len(k) for k in edofs[i].values()):
+                dg = False
+                break
+        return ElementContinuity(dg, dg, dg)
+
+
+def defined_on_dg_function_space(function):
+    """ Test whether the provided function is defined on a discontinuous
+    functionspace (and return true if it is).
+    """
+    return element_continuity(function.function_space().fiat_element).dg
+
+
+def get_function_space_degree(function):
+    """ Get the degree of the functionspace on which the provided function is
+    defined.
+    """
+    return function.function_space().ufl_element().degree()
+
+
+def get_function_space(function):
+    """ Return a tuple of CG / DG and the element degree for the functionspace
+    upon which the provided element is defined.
+    """
+    fs = ['CG']
+    if defined_on_dg_function_space(function):
+        fs[0] = 'DG'
+    fs.append(get_function_space_degree(function))
+    return fs
+
+
+# # # # # # # # # # #
+# P R O F I L I N G #
+# # # # # # # # # # #
+
+def profile(func):
+    """ Decorator to automatically label stages for the pyopt profiler
+    """
+    def func_wrapper(*args, **kwargs):
+        with timed_stage('{}'.format(func.__name__[:15])):
+            return func(*args, **kwargs)
+    return func_wrapper
+
+
+# # # # # # # # # # # # # # # # # # # # #
+#   P Y T H O N   E X T E N S I O N S   #
+# # # # # # # # # # # # # # # # # # # # #
+
+def import_path(fullpath):
+    """
+    Import a file with full path specification. Allows one to
+    import from anywhere, something __import__ does not do.
+    """
+    # TODO check this out and see if it works
+    path, filename = os.path.split(fullpath)
+    filename, ext = os.path.splitext(filename)
+    sys.path.append(path)
+    module = __import__(filename)
+    reload(module) # Might be out of date
+    del sys.path[-1]
+    return module
+
+
+class FrozenClass(object):
+    """ A class which can be (un-)frozen. If the class is frozen, no attributes
+        can be added to the class. """
+
+    __isfrozen = True
+
+    def __setattr__(self, key, value):
+        if self.__isfrozen and not hasattr(self, key):
+            raise TypeError("\n%r is a frozen class. \n%r is not a valid" \
+                            " attribute.\n" % (self, key))
+
+        object.__setattr__(self, key, value)
+
+    def _freeze(self):
+        """ Disallows adding new attributes. """
+        self.__isfrozen = True
+
+    def _unfreeze(self):
+        """ Allows adding new attributes. """
+        self.__isfrozen = False
+
+    def _convert_type(self, k):
+        attr = getattr(self, k)
+
+        if isinstance(attr, bool):
+            return attr
+        try:
+            return float(attr)
+        except:
+            return str(attr)
+
+    def __str__(self):
+        attrs = dir(self)
+        attrs_dict = {}
+
+        for k in attrs:
+            if k.startswith("_"):
+                continue
+            val = self._convert_type(k)
+            attrs_dict[k] = val
+
+        return yaml.dump(attrs_dict, default_flow_style=False)
---
+++ peryton/solvers/velocity_pressure_solver.py
@@ -0,0 +1,60 @@
+"""
+.. module:: velocity_pressure_solver
+   :synopsis: A module holding the forms, problem and solver for the velocity /
+    pressure equations.
+
+"""
+
+from firedrake import *
+from ..helpers import *
+
+
+class VelocityPressureSolver(object):
+    """ A class holding the forms, problem and solver for the velocity /
+    pressure equations.
+    """
+
+    def __init__(self, domain, velocity_function_space, pressure_function_space,
+                 boundary_conditions, body_forces, background_viscosity,
+                 turbulent_viscosity=Constant(0), solver_params='direct'):
+
+        self.domain = domain
+        self.mesh = domain.mesh
+        self.V = velocity_function_space
+        self.Q = pressure_function_space
+        self.bcs = boundary_conditions
+        self.body_forces = body_forces
+        self.nu_bg = background_viscosity
+        self.nu_T = turbulent_viscosity
+        self.solver_params = solver_params
+
+
+    def get_solvers(self):
+        """ Makes the linear variational solvers for the velocity / pressure
+        solve.
+        """
+        # Get the problem
+        self.get_problems():
+        # Get the solver parameters
+        self.get_petsc_params():
+        # Define the solver
+        self.solver = LinearVariationalSolver(self.problem,
+                                             solver_parameters=self.petsc_params
+                                             options_prefix='TODO_')
+
+
+    def get_problems(self):
+        """ Makes the linear variational problems for the velocity / pressure
+        solve.
+        """
+        # Get the variational forms of the equations
+        self.get_forms()
+        # Define the problem
+        self.problem = LinearVariationalProblem(lhs_a, rhs_L, soln_placeholder,
+                                                constant_jacobian=False)
+
+
+    def get_forms(self):
+        """ Make the variational forms for the velocity / pressure solve.
+        """
+        pass
